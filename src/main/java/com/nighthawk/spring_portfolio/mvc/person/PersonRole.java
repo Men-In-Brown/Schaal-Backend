9c684a1b6a2750dac1fad9fb34a6655717023f3c
tanayp327 <tanaypatel327@gmail.com>
2024-04-30 22:48:33 -0700

    changing the Id import to jakarta

--- src/main/java/com/nighthawk/spring_portfolio/mvc/person/PersonRole.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/PersonRole.java
@@ -3,11 +3,11 @@ package com.nighthawk.spring_portfolio.mvc.person;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Data
 @NoArgsConstructor
@@ -28,9 +28,10 @@ public class PersonRole {
     public static PersonRole[] init() {
         PersonRole student = new PersonRole("ROLE_STUDENT");
         PersonRole teacher = new PersonRole("ROLE_TEACHER");
+        PersonRole employer = new PersonRole("ROLE_EMPLOYER");
         PersonRole admin = new PersonRole("ROLE_ADMIN");
 
-        PersonRole[] initArray = {student, teacher, admin};
+        PersonRole[] initArray = {student, teacher, admin, employer};
         return initArray;
     }
 }
