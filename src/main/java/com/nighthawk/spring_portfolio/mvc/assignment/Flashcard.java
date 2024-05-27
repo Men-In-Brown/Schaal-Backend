8a5e03c8817940693a9252f2a692194293dc7f10
BobTheFarmer <colinmweis@gmail.com>
2024-05-27 11:18:35 -0700

    Flashcards for assignments

--- src/main/java/com/nighthawk/spring_portfolio/mvc/assignment/Flashcard.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/assignment/Flashcard.java
@@ -1,5 +1,6 @@
 package com.nighthawk.spring_portfolio.mvc.assignment;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,13 +12,11 @@ import java.util.Map;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
-import jakarta.persistence.*;
-
 @Data  // Annotations to simplify writing code (ie constructors, setters)
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity // Annotation to simplify creating an entity, which is a lightweight persistence domain object. Typically, an entity represents a table in a relational database, and each entity instance corresponds to a row in that table.
-public class Quiz {
+public class Flashcard {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -32,6 +31,9 @@ public class Quiz {
     private int maxPoints;
 
     @Column()
+    private int reqCards;
+
+    @Column()
     private String type;
 
     @Column()
@@ -40,29 +42,24 @@ public class Quiz {
     @Column()
     private LocalDateTime due;
 
-    /* HashMap is used to store JSON for daily "stats"
-    "stats": {
-        "2022-11-13": {
-            "calories": 2200
-            "steps": 8000
-        }
-    }
-    */
     @JdbcTypeCode(SqlTypes.JSON)
     @Column(columnDefinition = "jsonb")
-    private Map<String,Map<String, Object>> submissions = new HashMap<>(); 
+    private Map<String, String> flashcards; // Key: front, Value: back
 
     @JdbcTypeCode(SqlTypes.JSON)
     @Column(columnDefinition = "jsonb")
-    private Map<String, Object> questions = new HashMap<>(); 
+    private Map<String, Integer> completions; // Key: username, Value: number of completed flashcards
 
-    public Quiz(String title, String desc, int maxPoints, int jointId, Map<String, Object> questions,  LocalDateTime due) {
+    public Flashcard(String title, String desc, int maxPoints, int jointId, Map<String, String> flashcards, LocalDateTime due, int reqCards) {
         this.title = title;
         this.desc = desc;
         this.maxPoints = maxPoints;
-        this.type = "quiz";
         this.jointId = jointId;
-        this.questions = questions;
+        this.flashcards = flashcards;
+        this.type = "flashcards";
         this.due = due;
+        this.reqCards = reqCards;
+        this.completions = new HashMap<>();
     }
+
 }
